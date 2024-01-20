0de3050613a05fbeb0fc599a55a7344e06e8436d
pguilhermef <pguilhermesantos@hotmail.com>
2024-01-19 23:10:49 -0300

    Create phrase before form

--- src/app/sections/Forms.js
+++ src/app/sections/Forms.js
@@ -2,8 +2,21 @@ import Form from "@/components/Form"
 
 function Forms() {
   return(
-    <div className="min-h-screen w-full flex items-center justify-center bg-slate-100 text-black">
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
       <div className="flex-col items-center justify-center">
+        <div className="flex-col justify-center items-center">
+          <div className="flex justify-center items-center text-red-600 text-7xl sm:text-8xl mb-4">
+            A Go2Pizza
+          </div>
+          <div className="text-center mx-4 sm:mx-12 sm:text-2xl sm:max-w-[80vh]">
+            <p className="text-black mb-2">
+              É mais do que uma franquia, somos parceiros comprometidos com o seu sucesso. Estamos sempre à disposição para oferecer suporte e orientação garantindo que você alcance todo o potencial do seu empreendimento. <strong>Junte-se a nós</strong> nesta jornada para redefinir o padrão de delivery de pizzas. Aproveite agora a oportunidade e seja um franqueado inovador da Go2Pizza.
+            </p>
+            <p className="text-red-600 font-extrabold my-3">
+              O SEU SUCESSO COMEÇA AQUI.
+            </p>
+          </div>
+        </div>
         <Form />
       </div>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,58 +1,61 @@
 const Form = () => {
   return (
-    <div className="flex flex-col flex-grow min-h-7/8 uppercase px-3 min-w-[50vh] sm:min-w-[80vh]">
-      <label className="block mb-2 text-sm font-bold text-red-600">
-        Nome*:
-      </label>
-      <input
-        type="text"
-        className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
-      />
+    <div className="flex justify-center items-center mx-4">
+      <div className="flex-col justify-center items-center flex-grow min-h-7/8 uppercase min-w-[50vh] sm:max-w-[80vh] mb-8 mt-2">
+        <label className="block mb-2 text-sm font-bold text-red-600">
+          Nome*:
+        </label>
+        <input
+          type="text"
+          className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
+        />
 
-      <label className="block mb-2 text-sm font-bold text-red-600">
-        Email*:
-      </label>
-      <input
-        type="email"
-        className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
-      />
+        <label className="block mb-2 text-sm font-bold text-red-600">
+          Email*:
+        </label>
+        <input
+          type="email"
+          className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
+        />
 
-      <label className="block mb-2 text-sm font-bold text-red-600">
-        Celular*:
-      </label>
-      <input
-        type="tel"
-        className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
-      />
+        <label className="block mb-2 text-sm font-bold text-red-600">
+          Celular*:
+        </label>
+        <input
+          type="tel"
+          className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
+        />
 
-      <label className="block mb-2 text-sm font-bold text-red-600">
-        Disponibilidade de Investimento*:
-      </label>
-      <input
-        type="text"
-        className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
-      />
+        <label className="block mb-2 text-sm font-bold text-red-600">
+          Disponibilidade de Investimento*:
+        </label>
+        <input
+          type="text"
+          className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
+        />
 
-      <label className="block mb-2 text-sm font-bold text-red-600">
-        Estado*:
-      </label>
-      <input
-        type="text"
-        className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
-      />
+        <label className="block mb-2 text-sm font-bold text-red-600">
+          Estado*:
+        </label>
+        <input
+          type="text"
+          className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
+        />
 
-      <label className="block mb-2 text-sm font-bold text-red-600">
-        Cidade*:
-      </label>
-      <input
-        type="text"
-        className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
-      />
+        <label className="block mb-2 text-sm font-bold text-red-600">
+          Cidade*:
+        </label>
+        <input
+          type="text"
+          className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
+        />
 
-      <div className="w-full mt-5 bg-red-600 px-5 py-5 rounded-full text-center text-white font-extrabold text-2xl">
-            QUERO SER UM FRANQUEADO
+        <div className="w-full mt-5 bg-red-600 px-5 py-5 rounded-full text-center text-white font-extrabold text-2xl">
+              QUERO SER UM FRANQUEADO
+        </div>
       </div>
     </div>
+    
   );
 };
 
