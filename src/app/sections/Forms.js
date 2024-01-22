93f089a007f833a89f9d47d822768f01aa55bda5
pguilhermef <pguilhermesantos@hotmail.com>
2024-01-22 19:43:05 -0300

    Add pizzas image at form

--- src/app/sections/Forms.js
+++ src/app/sections/Forms.js
@@ -1,26 +1,29 @@
-import Form from "@/components/Form"
+import Form from "@/components/Form";
+import Pizzas from "@/images/pizzas.jpg";
+import Image from "next/image";
 
 function Forms() {
-  return(
-    <div className="min-h-screen min-w-screen flex items-center justify-center bg-slate-100">
-      <div className="flex-col items-center justify-center">
-        <div className="flex-col justify-center items-center">
-          <div className="flex justify-center items-center text-red-600 text-5xl sm:text-7xl md:text-8xl mb-4 mx-2 font-goToPizza font-bold">
-            A Go2Pizza
-          </div>
-          <div className="text-center mx-4 sm:mx-12 sm:text-2xl sm:max-w-[80vh]">
-            <p className="text-black mb-2">
-              É mais do que uma franquia, somos parceiros comprometidos com o seu sucesso. Estamos sempre à disposição para oferecer suporte e orientação garantindo que você alcance todo o potencial do seu empreendimento. <strong>Junte-se a nós</strong> nesta jornada para redefinir o padrão de delivery de pizzas. Aproveite agora a oportunidade e seja um franqueado inovador da Go2Pizza.
-            </p>
-            <p className="text-red-600 font-extrabold my-3">
-              O SEU SUCESSO COMEÇA AQUI.
-            </p>
-          </div>
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-100">
+      <div className="w-full max-w-3xl mx-auto mb-8">
+        <Image src={Pizzas} />
+      </div>
+      <div className="flex flex-col items-center justify-center ">
+        <div className="text-red-600 text-5xl sm:text-7xl md:text-8xl mb-4 font-goToPizza font-bold">
+          A Go2Pizza
         </div>
-        <Form />
+        <div className="mx-4 sm:mx-12 text-md sm:text-2xl max-w-3xl">
+          <p className="text-black mb-4 text-center">
+            É mais do que uma franquia, somos parceiros comprometidos com o seu sucesso. Estamos sempre à disposição para oferecer suporte e orientação garantindo que você alcance todo o potencial do seu empreendimento. <strong>Junte-se a nós</strong> nesta jornada para redefinir o padrão de delivery de pizzas. Aproveite agora a oportunidade e seja um franqueado inovador da Go2Pizza.
+          </p>
+          <p className="text-red-600 font-extrabold my-3 md:text-3xl text-center">
+            O SEU SUCESSO COMEÇA AQUI.
+          </p>
+        </div>
+        <Form/>
       </div>
     </div>
-  )
+  );
 }
 
-export default Forms
+export default Forms;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 const Form = () => {
   return (
-    <div className="flex justify-center items-center mx-4">
-      <div className="flex-col justify-center items-center flex-grow min-h-7/8 uppercase min-w-[40vh] sm:max-w-[80vh] mb-8 mt-2 mx-4 text-black">
+    <div className="flex justify-center items-center mx-4 w-full">
+      <div className="flex-col justify-center items-center w-full max-w-full mb-8 mt-2 mx-4 text-black">
         <label className="block mb-2 text-sm font-bold text-red-600">
           Nome*:
         </label>
@@ -27,21 +27,21 @@ const Form = () => {
         />
 
         <label className="block mb-2 text-sm font-bold text-red-600">
-        Disponibilidade de Investimento*:
-      </label>
-      <select
-        className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
-      >
-        <option value="10000-20000">R$10.000 - R$20.000</option>
-        <option value="20000-30000">R$20.000 - R$30.000</option>
-        <option value="30000-40000">R$30.000 - R$40.000</option>
-        <option value="40000-50000">R$40.000 - R$50.000</option>
-        <option value="50000-60000">R$50.000 - R$60.000</option>
-        <option value="60000-70000">R$60.000 - R$70.000</option>
-        <option value="70000-80000">R$70.000 - R$80.000</option>
-        <option value="80000-90000">R$80.000 - R$90.000</option>
-        <option value="90000-100000">R$90.000 - R$100.000</option>
-      </select>
+          Disponibilidade de Investimento*:
+        </label>
+          <select
+          className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
+        >
+          <option value="10000-20000">R$10.000 - R$20.000</option>
+          <option value="20000-30000">R$20.000 - R$30.000</option>
+          <option value="30000-40000">R$30.000 - R$40.000</option>
+          <option value="40000-50000">R$40.000 - R$50.000</option>
+          <option value="50000-60000">R$50.000 - R$60.000</option>
+          <option value="60000-70000">R$60.000 - R$70.000</option>
+          <option value="70000-80000">R$70.000 - R$80.000</option>
+          <option value="80000-90000">R$80.000 - R$90.000</option>
+          <option value="90000-100000">R$90.000 - R$100.000</option>
+        </select>
 
         <label className="block mb-2 text-sm font-bold text-red-600">
           Estado*:
@@ -59,12 +59,11 @@ const Form = () => {
           className="w-full px-3 py-2 mb-4 border rounded-full focus:outline-none focus:border-2 border-red-700 transition-all duration-300"
         />
 
-        <div className="w-full mt-5 bg-red-600 px-5 py-5 rounded-full text-center text-white font-extrabold text-2xl">
-              QUERO SER UM FRANQUEADO
+        <div className="w-full mt-5 bg-red-600 px-5 py-3 rounded-full text-center text-white font-extrabold text-lg">
+          QUERO SER UM FRANQUEADO
         </div>
       </div>
     </div>
-    
   );
 };
 
