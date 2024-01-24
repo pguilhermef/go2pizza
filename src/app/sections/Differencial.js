9c20281cb4f5c2cdafc847d77e6cab30413d233f
pguilhermef <pguilhermesantos@hotmail.com>
2024-01-23 23:57:53 -0300

    Creates grid

--- src/app/sections/Differencial.js
+++ src/app/sections/Differencial.js
@@ -1,45 +1,50 @@
 import Card from '@/components/Card';
 
+import delivery_costumer from '@/images/delivery.png';
+import delivery_truck from '@/images/fast-delivery.png';
+import quality from '@/images/quality.png';
+import money from '@/images/save-money.png';
+
+
 const cardsData = [
   {
     id: 1,
-    image: 'URL_DA_IMAGEM_1',
-    text: 'Frase do Card 1',
+    image: money,
+    text: 'Uma proposta financeira atraente e com baixo custo de investimento.',
   },
   {
     id: 2,
-    image: 'URL_DA_IMAGEM_2',
-    text: 'Frase do Card 2',
+    image: delivery_truck,
+    text: 'Deliveryprojetadopara seromaisrápidodo mercadodepizzas',
   },
   {
     id: 3,
-    image: 'URL_DA_IMAGEM_3',
-    text: 'Frase do Card 3',
+    image: quality,
+    text: 'Uma proposta financeira atraente e com baixo custo de investimento.',
   },
   {
     id: 4,
-    image: 'URL_DA_IMAGEM_4',
-    text: 'Frase do Card 4',
+    image: delivery_costumer,
+    text: 'Uma proposta financeira atraente e com baixo custo de investimento.',
   },
 ];
 
 
 function Differencial() {
-  return(
+  return (
     <div className="min-h-screen flex-col justify-center items-center bg-red-600">
       <div className="flex-col flex justify-center items-center text-center bg-go2pizza-yellow h-1/6 w-full text-red-600">
-        <div className="h-2/6 text-3xl font-light">O QUE NOS</div>
+        <div className="h-1/6 text-3xl font-light">O QUE NOS</div>
         <div className="h-4/6 text-7xl font-bold">DIFERENCIA?</div>
       </div>
 
-      <div className="grid grid-cols-2 grid-rows-2 gap-4 justify-center bg-blue-600">
-  {cardsData.map((card) => (
-    <Card key={card.id} image={card.image} text={card.text} />
-  ))}
-</div>
-
+      <div className="grid grid-cols-2 grid-rows-2 gap-4 justify-center items-center p-4 bg-blue-600 w-full">
+        {cardsData.map((card) => (
+          <Card key={card.id} image={card.image} text={card.text} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Differencial
+export default Differencial;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
-
+// components/Card.js
 const Card = ({ image, text }) => {
   return (
-    <div className="flex items-center border p-4 w-[40%] bg-red-200">
-      <img src={image} alt="Card Image" className="w-16 h-16 rounded-full mr-4" />
-      <p>{text}</p>
+    <div className="flex flex-col sm:flex-row justify-start items-center rounded-2xl p-4 bg-red-200">
+      <img src={image} alt="Card Image" className="min-h-16 w-2/6" />
+      <p className="px-4 text-center sm:text-left">{text}</p>
     </div>
   );
 };
