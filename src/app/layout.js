ca5dc919aeae666a174cb419dbb190e16fba117b
pguilhermef <pguilhermesantos@hotmail.com>
2024-01-30 05:37:20 -0300

    Change to correct title of page

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Go2Pizza',
+  title: 'Franquias | Go2Pizza',
   description: 'Go2Pizza franchise page',
 }
 
